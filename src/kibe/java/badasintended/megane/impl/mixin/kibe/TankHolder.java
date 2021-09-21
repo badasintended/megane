e97873715dbee9975764eeea8a77d75c4e37c8db
deirn <deirn@bai.lol>
2021-09-21 12:34:57 +0700

    update kibe

---
+++ src/kibe/java/badasintended/megane/impl/mixin/kibe/TankHolder.java
@@ -0,0 +1,24 @@
+package badasintended.megane.impl.mixin.kibe;
+
+import io.github.lucaargolo.kibe.blocks.entangledtank.EntangledTankEntity;
+import io.github.lucaargolo.kibe.blocks.miscellaneous.FluidHopperBlockEntity;
+import io.github.lucaargolo.kibe.blocks.tank.TankBlockEntity;
+import io.github.lucaargolo.kibe.blocks.vacuum.VacuumHopperEntity;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin({
+    TankBlockEntity.class,
+    EntangledTankEntity.class,
+    FluidHopperBlockEntity.class,
+    VacuumHopperEntity.class
+})
+public interface TankHolder {
+
+    @Invoker("getTank")
+    @SuppressWarnings({"deprecation", "UnstableApiUsage"})
+    SingleVariantStorage<FluidVariant> megane_getTank();
+
+}
--- src/kibe/kotlin/badasintended/megane/impl/Kibe.kt
+++ src/kibe/kotlin/badasintended/megane/impl/Kibe.kt
@@ -1,16 +1,31 @@
 package badasintended.megane.impl
 
-import alexiil.mc.lib.attributes.fluid.volume.FluidKeys
 import badasintended.megane.api.provider.FluidInfoProvider
+import badasintended.megane.api.provider.FluidProvider
 import badasintended.megane.api.registry.MeganeClientRegistrar
+import badasintended.megane.api.registry.MeganeRegistrar
+import badasintended.megane.impl.mixin.kibe.TankHolder
 import badasintended.megane.kotlin.api.KMeganeModule
-import io.github.lucaargolo.kibe.fluids.miscellaneous.ModdedFluid
+import badasintended.megane.kotlin.api.fluid
+import io.github.lucaargolo.kibe.fluids.LIQUID_XP
+import io.github.lucaargolo.kibe.fluids.miscellaneous.LiquidXpFluid
+
+inline val TankHolder.tank get() = megane_getTank()!!
 
 class Kibe : KMeganeModule {
-    override fun MeganeClientRegistrar.client() {
-        fluid(ModdedFluid::class.java, FluidInfoProvider.of(
-            { FluidKeys.get(it).name.style.color!!.rgb },
-            { FluidKeys.get(it).name }
+
+    @Suppress("UnstableApiUsage")
+    override fun MeganeRegistrar.common() {
+        fluid(TankHolder::class, FluidProvider.of(
+            { 1 },
+            { it, _ -> it.tank.variant.fluid },
+            { it, _ -> it.tank.amount / 81.0 },
+            { it, _ -> it.tank.capacity / 81.0 }
         ))
     }
+
+    override fun MeganeClientRegistrar.client() {
+        fluid(LiquidXpFluid::class, FluidInfoProvider.of(0x55FF55, LIQUID_XP.fluidBlock.name))
+    }
+
 }
