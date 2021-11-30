f4a91a2da681ddbfc16a7407299670b89bafba91
deirn <deirn@bai.lol>
2021-11-30 23:04:45 +0700

    modern-industrialization

--- src/modern-industrialization/java/badasintended/megane/impl/mixin/modern_industrialization/EnergyInputsComponentHolder.java
+++ src/modern-industrialization/java/badasintended/megane/impl/mixin/modern_industrialization/EnergyInputsComponentHolder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import aztech.modern_industrialization.machines.blockentities.multiblocks.DistillationTowerBlockEntity;
 import aztech.modern_industrialization.machines.blockentities.multiblocks.ElectricBlastFurnaceBlockEntity;
 import aztech.modern_industrialization.machines.blockentities.multiblocks.ElectricCraftingMultiblockBlockEntity;
+import aztech.modern_industrialization.machines.blockentities.multiblocks.FusionReactorBlockEntity;
 import aztech.modern_industrialization.machines.components.EnergyComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -12,7 +13,8 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin({
     DistillationTowerBlockEntity.class,
     ElectricBlastFurnaceBlockEntity.class,
-    ElectricCraftingMultiblockBlockEntity.class
+    ElectricCraftingMultiblockBlockEntity.class,
+    FusionReactorBlockEntity.class
 })
 public interface EnergyInputsComponentHolder {
 
